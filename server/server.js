40ba0b05f9825677945c759ce142f5d34b362ee7
Edward Park <ed.sh.park@gmail.com>
2016-11-18 21:03:55 -0800

    connected to heroku PG

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+const { write } = require('./eventCtrl');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(3000, () => console.log('Listening at 3000'));
+app.post('/write', write);
+
+
+
+
+io.on('connection', (socket) => {
+  // console.log('a user connected!');
+  socket.on('disconnect', () => {
+    // console.log('user disconnected!');
+  });
+  socket.on('chat message', (msg) => {
+    
+  })
+})
+
+http.listen(3000, () => console.log('Listening at 3000'));
