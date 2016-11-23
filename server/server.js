ae49363b6c4932ca8a764f38a35351ea8316d745
Edward Park <ed.sh.park@gmail.com>
2016-11-22 20:34:58 -0800

    updated with socket writes to DB

--- server/server.js
+++ server/server.js
@@ -5,27 +5,36 @@ const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const { write } = require('./eventCtrl');
+const { addToDb, showAll } = require('./eventCtrl');
 
+
+app.use(express.static(path.join(__dirname, '../'))); // need this to be able to serve up app.js + styles.css
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+// Routing --------------------------------
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.post('/write', write);
-
-
+// app.post('/addToDb', addToDb);
 
 
 io.on('connection', (socket) => {
-  // console.log('a user connected!');
+  console.log('a user connected!');
+  showAll();
+  
   socket.on('disconnect', () => {
     // console.log('user disconnected!');
   });
-  socket.on('chat message', (msg) => {
-    
-  })
+
+  socket.on('text entry', (msg) => {
+    addToDb(msg);
+    // io.emit('text receieved', msg);
+    socket.broadcast.emit('text receieved', msg);
+  });
+
 })
 
-http.listen(3000, () => console.log('Listening at 3000'));
+http.listen(3000, () => console.log('✌️  on 3000'));
